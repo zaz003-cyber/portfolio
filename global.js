9ec6f2eddbeebb0fa04dbf80c7591cca11dffe72
Zihan Zhang <zaz003@ucsd.edu>
2026-04-17 22:17:28 -0700

    Update global.js

--- global.js
+++ global.js
@@ -4,6 +4,8 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
+let BASE_PATH = "/portfolio/";
+
 let pages = [
   { url: "", title: "Home" },
   { url: "projects/", title: "Projects" },
@@ -19,5 +21,19 @@ for (let p of pages) {
   let url = p.url;
   let title = p.title;
 
-  nav.insertAdjacentHTML("beforeend", `<a href="${url}">${title}</a>`);
+  url = !url.startsWith("http") ? BASE_PATH + url : url;
+
+  let a = document.createElement("a");
+  a.href = url;
+  a.textContent = title;
+
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add("current");
+  }
+
+  if (a.host !== location.host) {
+    a.target = "_blank";
+  }
+
+  nav.append(a);
 }
