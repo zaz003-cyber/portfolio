4bd77620cc839f0e7307546167d0b0917c1ded42
Zihan Zhang <zaz003@ucsd.edu>
2026-04-17 22:39:01 -0700

    updated

--- global.js
+++ global.js
@@ -38,3 +38,23 @@ for (let p of pages) {
 
   nav.append(a);
 }
+
+document.body.insertAdjacentHTML(
+  "afterbegin",
+  `
+    <label class="color-scheme">
+      Theme:
+      <select>
+        <option value="light dark">Automatic</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+    </label>
+  `
+);
+
+let select = document.querySelector(".color-scheme select");
+
+select.addEventListener("input", function (event) {
+  document.documentElement.style.setProperty("color-scheme", event.target.value);
+});
