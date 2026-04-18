978d4dacef370967d1901ac857b6334236e9e788
Zihan Zhang <zaz003@ucsd.edu>
2026-04-17 21:56:12 -0700

    updated

--- global.js
+++ global.js
@@ -3,3 +3,6 @@ console.log("IT'S ALIVE!");
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+let currentLink = document.querySelector(`a[href="${location.pathname}"]`);
+currentLink?.classList.add("current");
