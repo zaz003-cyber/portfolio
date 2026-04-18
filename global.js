4662721766030458d1ff9ca8ecc9508d7e50ebaa
Zihan Zhang <zaz003@ucsd.edu>
2026-04-17 21:52:54 -0700

    Update global.js

--- global.js
+++ global.js
@@ -1,4 +1,5 @@
 console.log("IT'S ALIVE!");
 
 function $$(selector, context = document) {
-  return Array.from(context.querySelectorAll(selector));}
+  return Array.from(context.querySelectorAll(selector));
+}
