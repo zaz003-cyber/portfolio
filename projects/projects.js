b4d3d316747409e5696c3bbb5c93b54dfcf463a3
Zihan Zhang <zaz003@ucsd.edu>
2026-04-24 20:57:54 -0700

    updated

---
+++ projects/projects.js
@@ -0,0 +1,9 @@
+import { fetchJSON, renderProjects } from '../global.js';
+
+const projects = await fetchJSON('../lib/projects.json');
+const projectsContainer = document.querySelector('.projects');
+const projectsTitle = document.querySelector('.projects-title');
+
+renderProjects(projects, projectsContainer, 'h2');
+
+projectsTitle.textContent = `Projects (${projects.length})`;
