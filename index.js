bbb7ee607ffe69c703cd6697c26ee8559a2fdd2c
Zihan Zhang <zaz003@ucsd.edu>
2026-05-08 23:18:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7/+esm';
 
 const projects = await fetchJSON('./lib/projects.json');
-const latestProjects = projects.slice(0, 3);
+
+const latestProjects = d3
+  .sort(projects, (a, b) => d3.descending(a.year, b.year))
+  .slice(0, 3);
 
 const projectsContainer = document.querySelector('.projects');
 
