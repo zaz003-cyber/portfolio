fdab428200d7cb1b88faf036243423290b0c67db
Zihan Zhang <zaz003@ucsd.edu>
2026-05-08 21:15:12 -0700

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -1,5 +1,12 @@
 async function loadData() {
-  const data = await d3.csv('loc.csv');
+  const data = await d3.csv('loc.csv', (row) => ({
+    ...row,
+    line: Number(row.line),
+    depth: Number(row.depth),
+    length: Number(row.length),
+    datetime: new Date(row.datetime),
+  }));
+
   console.log(data);
 }
 
