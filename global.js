9aa5735a8c651ce52cde7f321ccd4c8f2d4cc3e3
Zihan Zhang <zaz003@ucsd.edu>
2026-05-08 20:53:41 -0700

    Update global.js

--- global.js
+++ global.js
@@ -14,6 +14,7 @@ let pages = [
   { url: "projects/", title: "Projects" },
   { url: "contact/", title: "Contact" },
   { url: "cv/", title: "CV" },
+  { url: 'meta/', title: 'Meta' },
   { url: "https://github.com/zaz003-cyber", title: "GitHub" },
 ];
 
