2c34f816a8d10391da8824f2da517800b96d85b5
Zihan Zhang <zaz003@ucsd.edu>
2026-05-08 21:43:23 -0700

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -82,3 +82,38 @@ function displayStats(data, commits) {
 }
 
 displayStats(data, commits);
+
+function renderScatterPlot(data, commits) {
+  const width = 1000;
+  const height = 600;
+
+  const svg = d3
+    .select('#chart')
+    .append('svg')
+    .attr('viewBox', `0 0 ${width} ${height}`)
+    .style('overflow', 'visible');
+
+  const xScale = d3
+    .scaleTime()
+    .domain(d3.extent(commits, (d) => d.datetime))
+    .range([0, width])
+    .nice();
+
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, 24])
+    .range([height, 0]);
+
+  const dots = svg.append('g').attr('class', 'dots');
+
+  dots
+    .selectAll('circle')
+    .data(commits)
+    .join('circle')
+    .attr('cx', (d) => xScale(d.datetime))
+    .attr('cy', (d) => yScale(d.hourFrac))
+    .attr('r', 5)
+    .attr('fill', 'steelblue');
+}
+
+renderScatterPlot(data, commits);
