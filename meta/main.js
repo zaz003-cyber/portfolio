4d3a17fede0f69a2f2bfd2a5db12309947edf496
Zihan Zhang <zaz003@ucsd.edu>
2026-05-22 23:01:41 -0700

    u

--- meta/main.js
+++ meta/main.js
@@ -317,6 +317,32 @@ function updateByProgress(progress) {
 
 displayStats(data, commits);
 initScatterPlot();
+
+d3.select('#scatter-story')
+  .selectAll('.step')
+  .data(commits)
+  .join('div')
+  .attr('class', 'step')
+  .html(
+    (d, i) => `
+      On ${d.datetime.toLocaleString('en', {
+        dateStyle: 'full',
+        timeStyle: 'short',
+      })},
+      I made <a href="${d.url}" target="_blank">${
+      i > 0 ? 'another glorious commit' : 'my first commit, and it was glorious'
+    }</a>.
+      I edited ${d.totalLines} lines across ${
+      d3.rollups(
+        d.lines,
+        (D) => D.length,
+        (d) => d.file,
+      ).length
+    } files.
+      Then I looked over all I had made, and I saw that it was very good.
+    `,
+  );
+
 updateByProgress(100);
 
 document
